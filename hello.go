804cb8c4fa284f6767c593110fbe3cb491578b84
Ryan Gregg <ryangregg@google.com>
2019-03-01 16:05:17 -0800

    Remove proj-id if we don't retrieve it

--- hello.go
+++ hello.go
@@ -2,78 +2,76 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
-// type Data struct {
-// 	Service  string
-// 	Revision string
-// 	Project  string
-// }
+type Data struct {
+	Service            string
+	Revision           string
+	Project            string
+	NetworkEgressError bool
+	ProjectFound       bool
+}
 
 func main() {
-	// tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl := template.Must(template.ParseFiles("index.html"))
 
 	// Get project ID from metadata server
-	// project := "???"
-	// client := &http.Client{}
-	// req, _ := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/project/project-id", nil)
-	// req.Header.Set("Metadata-Flavor", "Google")
-	// res, err := client.Do(req)
-	// if err == nil {
-	// 	defer res.Body.Close()
-	// 	responseBody, err := ioutil.ReadAll(res.Body)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	project = string(responseBody)
-	// } else {
-	// 	log.Print(err)
-	// }
+	project := "???"
+	projectFound := false
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/project/project-id", nil)
+	req.Header.Set("Metadata-Flavor", "Google")
+	res, err := client.Do(req)
+	if err == nil {
+		defer res.Body.Close()
+		responseBody, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		project = string(responseBody)
+		projectFound = true
+	}
 
-	// service := os.Getenv("K_SERVICE")
-	// if service == "" {
-	// 	service = "???"
-	// }
+	// Check for network egress configuration (CR-GKE)
+	networkEgressError := false
+	if projectFound == false {
+		// Check to see if we can reach something off the cluster e.g. www.google.com
+		req, _ = http.NewRequest("HEAD", "https://www.google.com", nil)
+		res, err = client.Do(req)
+		if err == nil && res.StatusCode >= 200 && res.StatusCode <= 299 {
+			// egress worked successfully
+			log.Print("Verified that network egress is working as expected.")
+		} else {
+			log.Print("Network egress appears to be blocked. Unable to access https://www.google.com.")
+			networkEgressError = true
+		}
+	}
 
-	// revision := os.Getenv("K_REVISION")
-	// if revision == "" {
-	// 	revision = "???"
-	// }
+	service := os.Getenv("K_SERVICE")
+	if service == "" {
+		service = "???"
+	}
 
-	// data := Data{
-	// 	Service:  service,
-	// 	Revision: revision,
-	// 	Project:  project,
-	// }
+	revision := os.Getenv("K_REVISION")
+	if revision == "" {
+		revision = "???"
+	}
+
+	data := Data{
+		Service:            service,
+		Revision:           revision,
+		Project:            project,
+		NetworkEgressError: networkEgressError,
+		ProjectFound:       projectFound,
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Query().Get("path")
-		metadataURL := "http://metadata.google.internal/computeMetadata/v1/"
-		if path != "" {
-			metadataURL = metadataURL + path
-		}
-
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", metadataURL, nil)
-		req.Header.Set("Metadata-Flavor", "Google")
-		res, err := client.Do(req)
-		if err == nil {
-			defer res.Body.Close()
-			responseBody, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				fmt.Fprint(w, err)
-			} else {
-				w.Header().Set("Content-Type", "application/json")
-				fmt.Fprint(w, responseBody)
-			}
-		} else {
-			w.Header().Set("Content-Type", "text/plain")
-			fmt.Fprint(w, err)
-		}
+		tmpl.Execute(w, data)
 	})
 
 	port := os.Getenv("PORT")
@@ -81,6 +79,6 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Hello from Cloud Run! The container started successfully and is listening for HTTP requests on $PORT: %s.", port)
+	log.Print("Hello from Cloud Run! The container started successfully and is listening for HTTP requests on $PORT.")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
