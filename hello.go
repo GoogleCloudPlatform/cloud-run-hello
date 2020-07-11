b2843ddf8a385af3de524bce88f78984344678d1
Steren <steren@google.com>
2020-07-11 09:25:29 -0700

    Display region (#29)

--- hello.go
+++ hello.go
@@ -21,50 +21,60 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Data struct {
 	Service      string
 	Revision     string
 	Project      string
-	ProjectFound bool
+	Region       string
 }
 
 func main() {
 	tmpl := template.Must(template.ParseFiles("index.html"))
 
 	// Get project ID from metadata server
-	project := "???"
-	projectFound := false
+	project := ""
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/project/project-id", nil)
 	req.Header.Set("Metadata-Flavor", "Google")
 	res, err := client.Do(req)
 	if err == nil {
 		defer res.Body.Close()
-		responseBody, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err)
+		if res.StatusCode == 200 {
+			responseBody, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			project = string(responseBody)
 		}
-		project = string(responseBody)
-		projectFound = true
+	}
+
+	// Get region from metadata server
+	region := ""
+	req, _ = http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/instance/region", nil)
+	req.Header.Set("Metadata-Flavor", "Google")
+	res, err = client.Do(req)
+	if err == nil {
+		defer res.Body.Close()
+		if res.StatusCode == 200 {
+			responseBody, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			region = regexp.MustCompile(`projects/[^/]*/regions/`).ReplaceAllString(string(responseBody), "")
+		}
 	}
 
 	service := os.Getenv("K_SERVICE")
-	if service == "" {
-		service = "???"
-	}
-
 	revision := os.Getenv("K_REVISION")
-	if revision == "" {
-		revision = "???"
-	}
 
 	data := Data{
 		Service:      service,
 		Revision:     revision,
 		Project:      project,
-		ProjectFound: projectFound,
+		Region:       region,
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
