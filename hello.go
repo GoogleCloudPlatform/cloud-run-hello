791d797adf27bf6dd406ec373cc5e088de15a18a
Julien Breux <julien.breux@gmail.com>
2024-07-12 18:48:08 +0200

    core: Upgrade deps, remove deprecated and bump Go version to 1.22.5 (#93)
    Signed-off-by: Julien Breux <julienbreux@google.com>

--- hello.go
+++ hello.go
@@ -20,7 +20,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -108,7 +108,7 @@ func main() {
 	if err == nil {
 		defer res.Body.Close()
 		if res.StatusCode == 200 {
-			responseBody, err := ioutil.ReadAll(res.Body)
+			responseBody, err := io.ReadAll(res.Body)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -124,7 +124,7 @@ func main() {
 	if err == nil {
 		defer res.Body.Close()
 		if res.StatusCode == 200 {
-			responseBody, err := ioutil.ReadAll(res.Body)
+			responseBody, err := io.ReadAll(res.Body)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -139,7 +139,7 @@ func main() {
 		if err == nil {
 			defer res.Body.Close()
 			if res.StatusCode == 200 {
-				responseBody, err := ioutil.ReadAll(res.Body)
+				responseBody, err := io.ReadAll(res.Body)
 				if err != nil {
 					log.Fatal(err)
 				}
